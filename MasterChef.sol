d58918f70de1e2b488fd08434f055097ddf7f20e
LionSoul99 <lionsoul99@protonmail.com>
2021-06-16 22:29:23 +0200

    changed referral additional mint to fee (no 101% mint)

--- MasterChef.sol
+++ MasterChef.sol
@@ -314,38 +314,32 @@ contract MasterChef is Referral, ReentrancyGuard {
 
             if(pool.harvestFee > 0){
                 fee = totalRewards.mul(pool.harvestFee).div(10000);
-                totalRewards = totalRewards.sub(fee);
             }
 
-            // Transfer the 'actual' reward to the user
-            safeGemstonesTransfer(msg.sender, totalRewards);
+            // Take referral cut from harvest -- either burn or distribute
+            uint256 referralCut = totalRewards.mul(referralCutPercentage).div(10000);
+            uint256 actualReward = totalRewards.sub(referralCut).sub(fee);
 
-            // Calculate Referral Cut and mint it
-            uint256 referralCut = 0;
-                                
+            require(referralCut + actualReward + fee == totalRewards, "Referral cut was not properly taken");
+
+            // Transfer the 'actual' reward to the user
+            safeGemstonesTransfer(msg.sender, actualReward);
+
             // Pay referral
             uint256 paidToReferrals = 0;
             
             // If referral is enabled, and the user is referred
             if(isReferralsEnabled() && hasReferrer(msg.sender)) {
-                referralCut = totalRewards.mul(referralCutPercentage).div(10000);
-                gemstones.mint(address(this), referralCut);
 
                 // Pay the referral cut (note: 60% to first level, 30% to second level, 10% to 3rd)
                 paidToReferrals = payReferral(msg.sender, referralCut);
                 uint256 rest = referralCut.sub(paidToReferrals);
                 // transfer the rest (if any) to fee address (if not all 3 levels are present)
                 safeGemstonesTransfer(gemstoneFeeAddr, rest.add(fee));
-
-                // Transfer the fee user paid back to user
-                if(paidToReferrals > 0) {
-                    safeGemstonesTransfer(msg.sender, paidToReferrals);
-                }
-            } else {
-                if(fee > 0 || referralCut > 0){
-                    // Transfer to fee address
-                    safeGemstonesTransfer(gemstoneFeeAddr, referralCut.add(fee));
-                }
+                
+            } else if(fee > 0 || referralCut > 0){
+                // Transfer to fee address
+                safeGemstonesTransfer(gemstoneFeeAddr, referralCut.add(fee));
             }
         }
         else if (pending > 0) {
